51a28d194fe795ee8c4bc9a386b700115783012c
Adanskie <adanskie2555@gmail.com>
2024-10-03 09:28:28 +0800

    akong first commit

---
+++ project.py/project2.py
@@ -0,0 +1,10 @@
+list_of_fruits = ["banana","apple","orange","mango"]
+
+user = input("Choice Apple : ")
+
+if user == list_of_fruits[0] :
+    print("banana exists")
+elif user == list_of_fruits[1] :
+    print("apple exists")
+else:
+    print("Not Exists")
